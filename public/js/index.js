aaa9be9e69d2cef69539e566b3a71b863db970e8
marcinjak9 <marcinjak9@gmail.com>
2017-03-27 22:55:50 +0200

    fixing index.js

---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+var socket = io();
+socket.on('connect', function() {
+	console.log('connected to server');
+});
+socket.on('disconnect', function() {
+	console.log('disconnected from server');
+});
+
+socket.on('newMessage', function(message){
+	console.log('new message:', message);
+});
