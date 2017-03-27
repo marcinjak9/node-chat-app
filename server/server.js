3f36d92f1f911a582b12d24bc386f5bc7758beec
marcinjak9 <marcinjak9@gmail.com>
2017-03-27 22:42:00 +0200

    add new message and create message event@

--- server/server.js
+++ server/server.js
@@ -13,11 +13,21 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  console.log('New user connected');
+	console.log('New user connected');
 
-  socket.on('disconnect', () => {
-    console.log('User was disconnected');
-  });
+	socket.emit('newMessage', {
+		from: "Mike",
+		text: "hey man!",
+		createAt: 2552
+	});
+
+	socket.on('createMessage', (message) => {
+		console.log('create message', message);
+	});
+
+	socket.on('disconnect', () => {
+	 console.log('User was disconnected');
+	});
 });
 
 
